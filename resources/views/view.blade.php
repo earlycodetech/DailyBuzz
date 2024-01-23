c6e56d7dc18fb2ca00752ce95eb69364ea1ba681
gab sinach26 <gabrielonyeka64@gmail.com>
2024-01-23 16:10:23 +0100

    viewpage2

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,8 +1,152 @@
 @extends('layouts.app')
 @section('content')
+    <section class="container h-100 " >
+
+      <div class="container -vh ">
+        <h3>
+          Once again Jermy Smith stunts the world with another wonderful production named:
+        </h3>
+        <h1 class="text-center fst-italic ">
+          THIS IS THE THE TITLE
+        </h1>
+        <h6>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis hic, dolorum nisi maiores deleniti possimus tenetur ad accusantium laboriosam eum laborum nemo, aut libero, modi recusandae aliquam! Aliquid debitis sunt ea a, pariatur ratione eos ad ex perferendis laboriosam, aspernatur aut placeat mollitia distinctio architecto dignissimos culpa laborum harum minus at iste! Vitae nisi, incidunt ipsum sapiente vero praesentium inventore qui cum quaerat optio eos voluptate at molestiae laudantium? Laudantium soluta at quis. Ipsam error voluptates, magnam amet in inventore necessitatibus porro consectetur, unde deleniti nihil corrupti, repellendus et nulla maxime quam animi minus vel modi sint omnis. Pariatur, velit!
+        </h6>
+       
+      </div>
+      <section>
+        <div class="container my-5">
+            <div class="row ps-5">
+              <div class="col-md-6 col-lg-4">
+                <div class="card border-0 shadow">
+                    <img src="{{ asset('images/video-bg.png') }}" class="card-img-top" alt="">
+
+                    <div class="card-body">
+                        <p class="h4 fw-semibold">
+                            This is the title
+                        </p>
+                        <p class="fw-semibold">
+                            Author: Jeremy Smith
+                        </p>
+                        <p class="fw-semibold">
+                            12th Jun. 2023
+                        </p>
+                        
+                        <div class="row">
+                          <div class="col-md-6 col-lg-4 bg-danger-subtle rounded-pill">
+                            <div class="card border-0 shadow bg-black rounded-pill">
+                              <a href="{{ url('view') }}" class="bg-danger-subtle text-center">
+                               Like
+                          </a>
+                          </div>
+                          </div>
+                          <div class="col-md-6 col-lg-4 bg-danger-subtle rounded-pill">
+                            <div class="card border-0 shadow bg-black rounded-pill">
+                              <a href="{{ url('view') }}" class="bg-danger-subtle text-center ">
+                              comment
+                          </a>
+                          </div>
+                          </div>
+                          <div class="col-md-6 col-lg-4 bg-danger-subtle rounded-pill">
+                          <div class="card border-0 shadow bg-danger-subtle rounded-pill">
+                            <a href="{{ url('view') }}" class="bg-danger-subtle text-center">
+                              share
+                          </a>
+                          </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+                <div class="col-md-6 col-lg-4">
+                    <div class="card border-0 shadow">
+                        <img src="{{ asset('images/video-bg.png') }}" class="card-img-top" alt="">
+
+                        <div class="card-body">
+                            <p class="h4 fw-semibold">
+                                This is the title
+                            </p>
+                            <p class="fw-semibold">
+                                Author: Jeremy Smith
+                            </p>
+                            <p class="fw-semibold">
+                                12th Jun. 2023
+                            </p>
+                            
+                            <div class="row">
+                              <div class="col-md-6 col-lg-4 bg-danger-subtle rounded-pill">
+                                <div class="card border-0 shadow bg-black rounded-pill">
+                                  <a href="{{ url('view') }}" class="bg-danger-subtle text-center">
+                                   Like
+                              </a>
+                              </div>
+                              </div>
+                              <div class="col-md-6 col-lg-4 bg-danger-subtle rounded-pill">
+                                <div class="card border-0 shadow bg-black rounded-pill">
+                                  <a href="{{ url('view') }}" class="bg-danger-subtle text-center ">
+                                  comment
+                              </a>
+                              </div>
+                              </div>
+                              <div class="col-md-6 col-lg-4 bg-danger-subtle rounded-pill">
+                              <div class="card border-0 shadow bg-danger-subtle rounded-pill">
+                                <a href="{{ url('view') }}" class="bg-danger-subtle text-center">
+                                  share
+                              </a>
+                              </div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                  <div class="card border-0 shadow">
+                      <img src="{{ asset('images/video-bg.png') }}" class="card-img-top" alt="">
+
+                      <div class="card-body">
+                          <p class="h4 fw-semibold">
+                              This is the title
+                          </p>
+                          <p class="fw-semibold">
+                              Author: Jeremy Smith
+                          </p>
+                          <p class="fw-semibold">
+                              12th Jun. 2023
+                          </p>
+                          
+                          <div class="row">
+                            <div class="col-md-6 col-lg-4 bg-danger-subtle rounded-pill">
+                              <div class="card border-0 shadow bg-black rounded-pill">
+                                <a href="{{ url('view') }}" class="bg-danger-subtle text-center">
+                                 Like
+                            </a>
+                            </div>
+                            </div>
+                            <div class="col-md-6 col-lg-4 bg-danger-subtle rounded-pill">
+                              <div class="card border-0 shadow bg-black rounded-pill">
+                                <a href="{{ url('view') }}" class="bg-danger-subtle text-center ">
+                                comment
+                            </a>
+                            </div>
+                            </div>
+                            <div class="col-md-6 col-lg-4 bg-danger-subtle rounded-pill">
+                            <div class="card border-0 shadow bg-danger-subtle rounded-pill">
+                              <a href="{{ url('view') }}" class="bg-danger-subtle text-center">
+                                share
+                            </a>
+                            </div>
+                            </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              
+    </section>
+    </section>
     <section class="container-fluid bg-danger-subtle "  >
+
         <div class="container">
-          <h3 class="text-center fst-italic text-bg-info  " > Dailybuzz</h3>
           <h3 class="text-center fst-italic " >TODAY'S BUZZ</h3>
           <div class="row ps-5 ">
             <div class="col-md-6  col-lg-4 md-3 p-3">
@@ -10,7 +154,7 @@
                 <img src="{{ asset('images/video-bg.png') }}" class="card-img-top" alt="">
                 <div class="card-body">
                  <h5>
-                    40 million missing as FG investigates 
+                    40 thrillion missing as FG investigates 
                  </h5>
                  <a href="">
                     readmore
