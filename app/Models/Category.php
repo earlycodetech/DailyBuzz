56286f48ea1306239a93e38ca4fcb3cc0614585b
emma2625 <emmanuelodobo10@gmail.com>
2024-01-23 16:34:42 +0100

    Update

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = "categories";
+    protected $fillble = [
+        'name',
+        'slug',
+    ];
+    
+    protected $hidden = [
+        'id'
+    ];
+
+
+    protected $casts = [
+        'id'=> 'integer',
+    ];
+}
